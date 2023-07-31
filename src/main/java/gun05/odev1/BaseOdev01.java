e41e606e8c284f353609e8d38159a3901ee05596
Hasan Ozyer <hasanozyer06@gmail.com>
2023-07-31 18:16:25 +0300

    locator changes implemented

---
+++ src/main/java/gun05/odev1/BaseOdev01.java
@@ -0,0 +1,49 @@
+package gun05.odev1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import utils.Driver;
+
+import java.time.Duration;
+
+public class BaseOdev01 {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeTest
+    public void setup() {
+        driver = Driver.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void sendKeys(By locator, String text) {
+        WebElement element = driver.findElement(locator);
+        sendKeys(element, text);
+
+    }
+
+    public void sendKeys(WebElement element, String text) {
+        element.sendKeys(text);
+    }
+
+    public void click(By locator) {
+        click(waitUntilClickable(locator));
+    }
+
+    public void click(WebElement element) {
+        element.click();
+    }
+
+    public WebElement waitUntilVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitUntilClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+}
