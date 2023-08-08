8b390ecbd1a4944102be160431702d8fedc56087
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-08 21:37:16 +0300

    TestNG extend reports base class

---
+++ src/main/java/gun07/xml1/Test11xml.java
@@ -0,0 +1,27 @@
+package gun07.xml1;
+
+import org.testng.annotations.Test;
+
+public class Test11xml {
+
+    @Test
+    public void test5() {
+        System.out.println("Test5");
+    }
+
+    @Test
+    public void test6() {
+        System.out.println("Test6");
+    }
+
+    @Test
+    public void test7() {
+        System.out.println("Test7");
+    }
+
+    @Test
+    public void test8() {
+        System.out.println("Test8");
+    }
+
+}
---
+++ src/main/java/gun07/xml3/Test32xml.java
@@ -0,0 +1,27 @@
+package gun07.xml3;
+
+import org.testng.annotations.*;
+
+public class Test32xml {
+
+
+    @Test
+    public void test5() {
+        System.out.println("Test5");
+    }
+
+    @Test
+    public void test6() {
+        System.out.println("Test6");
+    }
+
+    @Test
+    public void test7() {
+        System.out.println("Test7");
+    }
+
+    @Test
+    public void test8() {
+        System.out.println("Test8");
+    }
+}
