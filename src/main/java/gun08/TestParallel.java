6318f0951856984ab9a1d68d1fe2d4499f8a7feb
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-09 23:44:44 +0300

    Added TestParallel class to execute parallel tests for OpenCart login and logout using the HomePage33 class.

---
+++ src/main/java/gun08/TestParallel.java
@@ -0,0 +1,24 @@
+package gun08;
+
+import org.testng.annotations.Test;
+
+public class TestParallel extends TestParallelBase {
+
+    HomePage33 hp;
+
+    @Test
+    public void test1() {
+        hp = new HomePage33(driver);
+        hp.goToUrl();
+    }
+
+    @Test(dependsOnMethods = {"test1"})
+    public void test2() {
+        hp.login();
+    }
+
+    @Test(dependsOnMethods = {"test2"})
+    public void test3() {
+        hp.logout();
+    }
+}
