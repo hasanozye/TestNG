10b5e595086647384b9831b64828eb2e2b930758
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-10 23:06:33 +0300

    The "DriverStatic" class provides a static WebDriver instance for browser automation, supporting various browser options such as Edge, Firefox, Safari, and Chrome, and includes configuration settings like remote debugging and starting maximized.

---
+++ src/main/java/utils/DriverStatic.java
@@ -0,0 +1,57 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class DriverStatic {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return getDriver(Browsers.CHROME);
+    }
+
+    public static WebDriver getDriver(Browsers browser) {
+        if (driver == null) {
+            switch (browser) {
+                case EDGE -> {
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                }
+                case FIREFOX -> {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                case SAFARI -> {
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                }
+                default -> {
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*"); //version 11 socket hatası için
+                    options.addArguments("--start-maximized"); // ilk açılışta maximized açılır
+//                    options.addArguments("--headless"); // arka planda çalışır
+//                    options.addArguments("--remote-debugging-port=9222");
+//                    options.addArguments("--user-data-dir=" + System.getProperty("user.home") + "\\Desktop\\User Data");
+//                    options.addArguments("--profile-directory=Profile 1");
+                    driver = new ChromeDriver(options);
+                }
+            }
+        }
+        return driver;
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            DriverStatic.getDriver().quit();
+        }
+    }
+
+
+}
