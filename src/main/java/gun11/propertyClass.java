2fdd330cd39b46543acce3f74934ba86c352ef7d
Hasan Ozyer <hasanozyer06@gmail.com>
2023-08-16 21:37:57 +0300

    Test class showcasing how to read properties from a file using Java's `Properties` class. It includes two test methods:
    1. `propertyFile()`: Demonstrates how to create a properties object, add key-value pairs, and retrieve values.
    2. `readPropertyFromFile()`: Shows how to read properties from a file named "config.properties" located in the specified path. It loads the properties from the file and retrieves values for URL, username, and password.
    
    The class uses exception handling for IO operations.

---
+++ src/main/java/gun11/propertyClass.java
@@ -0,0 +1,42 @@
+package gun11;
+
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class propertyClass {
+
+    //    read property file
+    @Test
+    public void propertyFile() {
+
+        // properties Classina ihtiyac var
+        Properties properties = new Properties(); //property nesnesi oluşturulur
+        properties.put("username", "myUser"); // nesneye ikili veri eklenir
+        properties.put("password", "myPass"); // nesneye ikili veri eklenir
+
+        String user = properties.getProperty("username"); // property den veri okuma
+        String pass = properties.getProperty("password"); // property den ver, okuma
+
+        System.out.println(user + " , " + pass);
+    }
+
+    @Test
+    public void readPropertyFromFile() throws IOException {
+
+        Properties properties = new Properties();
+        FileReader fileReader = new FileReader("src/main/java/gun11/config.properties");
+        properties.load(fileReader);
+
+        String url = properties.getProperty("url");
+        String user = properties.getProperty("username");
+        String pass = properties.getProperty("password");
+
+        System.out.println(url);
+        System.out.println(user);
+        System.out.println(pass);
+
+    }
+}
